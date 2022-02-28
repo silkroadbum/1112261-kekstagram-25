9043845f5c796d197b54e6551fab530b531a82d2
Anatolii Egorov <silkroadbum@mail.ru>
2022-02-28 20:05:49 +0500

    Поправил недочёты, выданные наставником!

--- js/main.js
+++ js/main.js
@@ -11,6 +11,10 @@ checkStringLength('Анатолий', 10);
 let idDescription = 0;
 
 const PHOTO_DESCRIPTION_COUNT = 25;
+const MIN_VALUE_OF_LIKES = 15;
+const MAX_VALUE_OF_LIKES = 200;
+const MIN_VALUE_AVATAR = 1;
+const MAX_VALUE_AVATAR = 6;
 
 const NAMES = [
   'Анатолий',
@@ -28,7 +32,7 @@ const NAMES = [
 const MESSAGES = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
-  'огда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
@@ -47,7 +51,7 @@ const getRandomArrayElement = (elements) => elements[getRandomInteger(0, element
 
 const createComment = () => ({
   id: idDescription,
-  avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+  avatar: `img/avatar-${getRandomInteger(MIN_VALUE_AVATAR, MAX_VALUE_AVATAR)}.svg`,
   message: getRandomArrayElement(MESSAGES),
   name: getRandomArrayElement(NAMES)
 });
@@ -58,7 +62,7 @@ const createPhotoDescription = () => {
     id: idDescription,
     url: `photos/${idDescription}.jpg`,
     description: getRandomArrayElement(DESCRIPTIONS),
-    likes: getRandomInteger(15, 200),
+    likes: getRandomInteger(MIN_VALUE_OF_LIKES, MAX_VALUE_OF_LIKES),
     comments: createComment()
   };
 };
