0638c7d9a3dca32dfe0a046335f4ea10ba6be8c4
Anatolii Egorov <silkroadbum@mail.ru>
2022-03-09 21:46:45 +0500

    Вынес константы в отдельный модуль, создал отдельный счетчик для id комментариев и сделал чтобы в каждом объекте был массив комментариев

--- js/util.js
+++ js/util.js
@@ -7,4 +7,20 @@ const checkStringLength = (testString, maxLength) => testString.length <= maxLen
 //Функция получения рандомного элемента из массива
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
-export {getRandomInteger, checkStringLength, getRandomArrayElement};
+const createIdGenerator = (min, max) => {
+  const previousValues = [];
+
+  return function () {
+    let curentValue = getRandomInteger(min, max);
+    if (previousValues.length >= (max - min + 1)) {
+      return null;
+    }
+    while (previousValues.includes(curentValue)) {
+      curentValue = getRandomInteger(min, max);
+    }
+    previousValues.push(curentValue);
+    return curentValue;
+  };
+};
+
+export {getRandomInteger, checkStringLength, getRandomArrayElement, createIdGenerator};
