120352c5ff06e93dc1a022ea96575a7b938ba5f0
Anatolii Egorov <silkroadbum@mail.ru>
2022-03-10 21:59:26 +0500

    Добавил функции создания элемента с классом и функцию создания комментария, в функции показа полноразмерного фото в цикле добавил комментарии к фотографии

--- js/modal.js
+++ js/modal.js
@@ -6,6 +6,7 @@ const bigPicture = modalWindow.querySelector('.big-picture__img');
 const likesCount = modalWindow.querySelector('.likes-count');
 const commentCount = socialCommentCount.querySelector('.comments-count');
 const descriptionFullPhoto = modalWindow.querySelector('.social__caption');
+const commentsList = document.querySelector('.social__comments');
 
 closeButton.addEventListener('click', () => {
   modalWindow.classList.add('hidden');
@@ -17,6 +18,23 @@ document.addEventListener('keydown', (evt) => {
   }
 });
 
+const makeElement = (tagName, className) => {
+  const element = document.createElement(tagName);
+  element.classList.add(className);
+  return element;
+};
+
+const createComment = () => {
+  const comment = makeElement('li', 'social__comment');
+  const avatar = makeElement('img', 'social__picture');
+  avatar.style.width = '35';
+  avatar.style.height = '35';
+  comment.appendChild(avatar);
+  const commentText = makeElement('p', 'social__text');
+  comment.appendChild(commentText);
+  return comment;
+};
+
 const showFullPhoto = (miniature, pictureElement) => {
   miniature.addEventListener('click', () => {
     modalWindow.classList.remove('hidden');
@@ -27,6 +45,12 @@ const showFullPhoto = (miniature, pictureElement) => {
     likesCount.textContent = pictureElement.likes;
     commentCount.textContent = pictureElement.comments.length;
     descriptionFullPhoto.textContent = pictureElement.description;
+    for (let i = 0; i < pictureElement.comments.length; i++) {
+      const newComment = createComment();
+      newComment.querySelector('.social__picture').src = pictureElement.comments[i].avatar;
+      newComment.querySelector('.social__text').textContent = pictureElement.comments[i].message;
+      commentsList.appendChild(newComment);
+    }
   });
 };
 
