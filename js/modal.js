a0e1aa1609d448b75d5b7f34c5864eebb7dc5d5f
Anatolii Egorov <silkroadbum@mail.ru>
2022-03-10 19:25:59 +0500

    Добавил код для прятания блока счетчика комментариев и блока загрузки новых комментариев после открытия модального окна

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,7 @@
 const modalWindow = document.querySelector('.big-picture');
 const closeButton = modalWindow.querySelector('.cancel');
-
+const commentCount = modalWindow.querySelector('.social__comment-count');
+const commentLoader = modalWindow.querySelector('.comments-loader');
 
 closeButton.addEventListener('click', () => {
   modalWindow.classList.add('hidden');
@@ -15,6 +16,9 @@ document.addEventListener('keydown', (evt) => {
 const showFullPhoto = (miniature) => {
   miniature.addEventListener('click', () => {
     modalWindow.classList.remove('hidden');
+    commentCount.classList.add('hidden');
+    commentLoader.classList.add('hidden');
+    document.body.classList.add('modal-open');
   });
 };
 
