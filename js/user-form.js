442da1bf3c5c3ecd3112fcd8bd999b12767f06ba
Anatolii Egorov <silkroadbum@mail.ru>
2022-03-13 22:34:42 +0500

    Сделал сброс значения поля загрузки новой фотографии

--- js/user-form.js
+++ js/user-form.js
@@ -1,11 +1,30 @@
+import {isEscapeKey} from './util.js';
+
 const formDownloadPicture = document.querySelector('.img-upload__form');
 const buttonUploadFile = document.querySelector('#upload-file');
 const formEditImage = document.querySelector('.img-upload__overlay');
 const buttonCloseFormEdit = document.querySelector('#upload-cancel');
 
+buttonCloseFormEdit.addEventListener('click', closeFormEdit);
+
+const onFormEditEscKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeFormEdit();
+  }
+};
+
+function closeFormEdit () {
+  formEditImage.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onFormEditEscKeydown);
+  buttonUploadFile.value = '';
+}
+
 buttonUploadFile.addEventListener('change', () => {
   formEditImage.classList.remove('hidden');
   document.body.classList.add('modal-open');
+  document.addEventListener('keydown', onFormEditEscKeydown);
 });
 
 const pristine = new Pristine(formDownloadPicture);
