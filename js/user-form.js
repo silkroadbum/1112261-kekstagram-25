5475f78d289dbd4310ccccdd04709e984ec074c9
Anatolii Egorov <silkroadbum@mail.ru>
2022-03-14 23:57:48 +0500

    Добавил функцию проверки хэштегов

--- js/user-form.js
+++ js/user-form.js
@@ -4,8 +4,8 @@ const formDownloadPicture = document.querySelector('.img-upload__form');
 const buttonUploadFile = document.querySelector('#upload-file');
 const formEditImage = document.querySelector('.img-upload__overlay');
 const buttonCloseFormEdit = document.querySelector('#upload-cancel');
-
-buttonCloseFormEdit.addEventListener('click', closeFormEdit);
+const buttonSubmitForm = formDownloadPicture.querySelector('img-upload__submit');
+const hashtagValue = formDownloadPicture.querySelector('.text__hashtags');
 
 const onFormEditEscKeydown = (evt) => {
   if (isEscapeKey(evt)) {
@@ -19,12 +19,14 @@ function closeFormEdit () {
   document.body.classList.remove('modal-open');
   document.removeEventListener('keydown', onFormEditEscKeydown);
   buttonUploadFile.value = '';
+  buttonCloseFormEdit.removeEventListener('click', closeFormEdit);
 }
 
 buttonUploadFile.addEventListener('change', () => {
   formEditImage.classList.remove('hidden');
   document.body.classList.add('modal-open');
   document.addEventListener('keydown', onFormEditEscKeydown);
+  buttonCloseFormEdit.addEventListener('click', closeFormEdit);
 });
 
 const pristine = new Pristine(formDownloadPicture);
@@ -33,9 +35,30 @@ formDownloadPicture.addEventListener('submit', (evt) => {
   evt.preventDefault();
 
   const isValid = pristine.validate();
-  if (isValid) {
-    throw 'Можно отправлять';
+  if (!isValid) {
+    buttonSubmitForm.disabled = true;
   } else {
-    throw 'Строка не валидна';
+    buttonSubmitForm.disabled = false;
   }
 });
+
+const checkHashtag = () => {
+  let isValidHashtag = true;
+  hashtagValue.addEventListener('input', () => {
+    const arrHashtag = hashtagValue.value.split(' ');
+    if (arrHashtag.lengt > 5) {
+      isValidHashtag = false;
+    } else if (arrHashtag.lengt <= 5 && arrHashtag.lengt >= 1){
+      arrHashtag.forEach((hashtag) => {
+        const re = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}/;
+        if (!re.test(hashtag)) {
+          isValidHashtag = false;
+        } else {
+          isValidHashtag = true;
+        }
+      });
+    }
+  });
+  return isValidHashtag;
+};
+checkHashtag();
