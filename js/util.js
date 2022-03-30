80fd4519262ca765b56152a1e43c83372f557c31
Anatolii Egorov <silkroadbum@mail.ru>
2022-03-30 19:31:29 +0500

    Добавил функцию устранения дребезга в util.js

--- js/util.js
+++ js/util.js
@@ -31,4 +31,13 @@ const showAlert = (message) => {
   }, ALERT_SHOW_TIME);
 };
 
-export {getRandomInteger, getRandomArrayElement, makeElement, isEscapeKey, isEnterKey, showAlert};
+//Функция устранения дребезга
+const debounce = (callback, timeoutDelay) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
+export {getRandomInteger, getRandomArrayElement, makeElement, isEscapeKey, isEnterKey, showAlert, debounce};
